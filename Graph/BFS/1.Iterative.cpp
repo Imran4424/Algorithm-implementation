6978f522d84a5d771afcdd879bcd0104bc848042
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-11 12:08:51 +0600

    BFS DFS

--- Graph/BFS/1.Iterative.cpp
+++ Graph/BFS/1.Iterative.cpp
@@ -19,6 +19,31 @@ void BFS(int startVertex, int totalVertex)
 	{
 		visited[i] = false;
 	}
+
+
+	queue <int> currentNodes;
+
+	visited[startVertex] = true;
+	currentNodes.push(startVertex);
+
+	list <int> ::iterator itr;
+
+	while(!currentNodes.empty())
+	{
+		int current = currentNodes.front();
+		cout << current << " ";
+
+		currentNodes.pop();
+
+		for( itr = adj[current].begin(); itr != adj[current].end(); itr++)
+		{
+			if (!visited[*itr])
+			{
+				visited[*itr] = true;
+				currentNodes.push(*itr);
+			}
+		}
+	}
 }
 
 
@@ -30,6 +55,17 @@ int main(int argc, char const *argv[])
 	cin >> vertex;
 
 	adj = new list <int> [vertex];  // taking an array of list (doubly link list)
+
+	AddEdge(1, 2); 
+	AddEdge(2, 3); 
+	AddEdge(0, 2); 
+	AddEdge(2, 0); 
+	AddEdge(3, 3); 
+	AddEdge(0, 1); 
+
+	cout << "Following is Breadth First Traversal "
+		<< "(starting from vertex 2) \n"; 
+	BFS(2, vertex); 
 	
 	return 0;
 }
