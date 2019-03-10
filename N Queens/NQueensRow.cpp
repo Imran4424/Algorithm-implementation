eebb29d9692055369322a584ba76e49fc8857ab8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-10 16:00:10 +0600

    n queen

--- N Queens/NQueensRow.cpp
+++ N Queens/NQueensRow.cpp
@@ -2,6 +2,73 @@
 using namespace std;
 
 
+bool IsSafe(int num, int board[num][num], int row, int col)
+{
+	int i,j;
+
+	// checking the column
+
+	for(i = 0; i < row; i++)
+	{
+		if (board[i][col])
+		{
+			return false;
+		}
+	}
+
+	// check upper diagonal on the left side
+
+	for(i = row-1,j = col-1; i>=0, j>=0; i--, j--)
+	{
+		if (board[i][j])
+		{
+			return false;
+		}
+	}
+
+	// check upper diagonal on the right side
+
+	for(i = row-1,j = col+1; i >= 0, j < num; i--, j++)
+	{
+		if (board[i][j])
+		{
+			return false;
+		}
+	}
+
+
+	return true;
+}
+
+
+bool Solve(int num, int board[num][num], int row)
+{
+	if(row == num) // base condition
+	{
+		return true;
+	}
+
+	for(int j = 0; j < num; i++)
+	{
+		if (IsSafe(num, board, row, j))
+		{
+			board[row][j] = 1;
+
+			if (Solve(num, board, row+1))
+			{
+				return true;
+			}
+
+			
+			board[row][j] = 0; // backtracking
+		}
+	}
+
+
+	return false; // when queen can't be placed in this row
+}
+
+
 int main(int argc, char const *argv[])
 {
 	cout << "How many queens you want to place" << endl;
@@ -9,6 +76,33 @@ int main(int argc, char const *argv[])
 	int num;
 	cin >> num;
 
+	int board[num][num];
+
+	for(int i = 0; i<num; i++)
+	{
+		for(int j=0; j<num; j++)
+		{
+			board[i][j] = 0;
+		}
+	}
+
+	if (Solve(num, board, 0))
+	{
+		cout << "Solution doesn't exist" << endl;
+	}
+	else
+	{
+		
+		for(int i = 0; i<num; i++)
+		{
+			for(int j=0; j<num; j++)
+			{
+				cout << board[i][j] << endl;
+			}
+
+			cout << endl;
+		}
+	}
 	
 
 	return 0;
