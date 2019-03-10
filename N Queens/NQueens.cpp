7120c2fb845ed5763e555470a2e16cbeae796ca5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-10 14:16:08 +0600

    n queen

---
+++ N Queens/NQueens.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	cout << "How many queens you want to place" << endl;
+
+	int num;
+	cin >> num;
+
+	return 0;
+}
