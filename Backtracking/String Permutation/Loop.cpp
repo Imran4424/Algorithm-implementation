02d371bcb48de1c196601e331c2d96982e208f2a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-17 19:35:47 +0600

    backtracking

--- Backtracking/String Permutation/Loop.cpp
+++ Backtracking/String Permutation/Loop.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void Permutation(string s, int start, int stop)
+{
+	if (start == stop)
+	{
+		cout << s << endl;
+	}
+	else
+	{
+		for (int i = start; i <= stop; ++i)
+		{
+			swap(s[start], s[i]);
+
+			Permutation(s, start+1, stop);
+
+			swap(s[start], s[i]); // backtrack
+		}
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	string s;
@@ -10,7 +29,7 @@ int main(int argc, char const *argv[])
 	cin >> s;
 
 	
-
+	Permutation(s, 0, s.size());
 
 	return 0;
 }
