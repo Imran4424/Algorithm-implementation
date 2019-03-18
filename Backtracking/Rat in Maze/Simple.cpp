8444a11d9c1a43ad5069816ccbd10bafb8a8dc98
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-18 07:58:33 +0600

    backtracking

--- Backtracking/Rat in Maze/Simple.cpp
+++ Backtracking/Rat in Maze/Simple.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int maze[4][4];
+int maze[32][32];
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the maze size in row and column" << endl;
+
+	int row, col;
+	cin >> row >> col;
+
+	return 0;
+}
 
