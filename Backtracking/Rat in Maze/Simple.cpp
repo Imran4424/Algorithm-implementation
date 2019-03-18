4657c6b0048e87c154f924784a07735ee554a389
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-18 09:02:53 +0600

    backtracking

--- Backtracking/Rat in Maze/Simple.cpp
+++ Backtracking/Rat in Maze/Simple.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int maze[32][32];
+
+
+
 
 int main(int argc, char const *argv[])
 {
@@ -9,6 +12,18 @@ int main(int argc, char const *argv[])
 
 	int row, col;
 	cin >> row >> col;
+
+	vector< vector <int> > maze(row, vector <int> (col));
+
+	vector< vector <int> > path(row, vector <int> (col, 0)); //initializing path matrix with zero
+
+	for (int i = 0; i < maze.size(); ++i)
+	{
+		for(int j = 0 ; j < maze[i].size(); ++j)
+		{
+			cin >> maze[i][j];
+		}
+	}
 
 	return 0;
 }
