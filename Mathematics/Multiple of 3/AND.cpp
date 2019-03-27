1f2067ef3be20f530f3081793b85b09d64c10bf7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-27 20:11:44 +0600

    mathematics

--- Mathematics/Multiple of 3/AND.cpp
+++ Mathematics/Multiple of 3/AND.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
 using namespace std;
 
+int IsMultipleOfThree(int n)
+{
+	int oddCount = 0;
+	int evenCount = 0;
+
+	/*
+		checking the number is negative is negative or not
+
+		if it is negative then making it positive
+	*/
+
+	if(n < 0)
+	{
+		n = -n;
+	}
+
+	if(n == 0)
+	{
+		return 1;
+	}
+
+	if(n == 1)
+	{
+		return 0;
+	}
+
+	while(n)
+	{
+		/*
+			if the odd bit is set then increment odd counter
+		*/
+
+		if (n & 1)
+		{
+			oddCount++;
+		}
+
+		n = n >> 1;
+
+		/*
+			if the even bit is set then increment odd counter
+		*/
+
+		if(n & 1)
+		{
+			evenCount++;
+		}
+
+		n = n >> 1;
+	}
+}
+
 
 int main(int argc, char const *argv[])
 {
 	
+
 	return 0;
 }
