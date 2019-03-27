e20626bea8d44c39c66533067cf7f6b03443bcc1
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-27 20:40:58 +0600

    mathematics

--- Mathematics/Bit Operations/MultipleOfSeven.cpp
+++ Mathematics/Bit Operations/MultipleOfSeven.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int MultipleOfSeven(int num)
+{
+	return ((num << 3) - num);
+}
 
 
 int main(int argc, char const *argv[])
 {
-	
+	int num;
+
+	cin >> num;
+
+	cout << "Multiple of seven of " << num << " is: " << MultipleOfSeven(num) << endl;
+
 	return 0;
 }
