58e2a37565f3e91b524f38fb3db91a3c007eb20a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-28 07:46:24 +0600

    mathematics

--- Mathematics/RecursiveMultipication.cpp
+++ Mathematics/RecursiveMultipication.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 using namespace std;
+
+
+typedef long long int lli;
+
+
+lli Multipication(lli x, lli y)
+{
+	/* Base Condition */
+
+	if(y == 0)
+	{
+		return 0;
+	}
+
+	/* Adding x one by one until y is 0 */
+
+	if(y > 0)
+	{
+		return (x + Multipication(x, y - 1));
+	}
+
+	/*  */
+
+	if (y < 0)
+	{
+		return - (Multipication(x, -y));
+	}
+}
