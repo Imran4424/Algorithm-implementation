da95b16c6aaf2240717509c41836b6d99a6ba77b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-28 07:17:24 +0600

    mathematics

--- Mathematics/BabylonianSquareRoot.cpp
+++ Mathematics/BabylonianSquareRoot.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+double BabylonianRoot(double num)
+{
+	double x = num;
+	double y = 1;
 
+	double e = 0.000001; /* e decides the accuracy level */
+
+	while(x - y > e)
+	{
+		x = (x + y)/2;
+		y = num / x;
+	}
+
+}
 
 
 int main(int argc, char const *argv[])
