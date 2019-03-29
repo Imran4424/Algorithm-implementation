8b8c201a3fa42254c69cec36587ade8a7f4263b3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-29 15:08:15 +0600

    mathematics

--- Mathematics/CountNumbers/Doesn'tContain3.cpp
+++ Mathematics/CountNumbers/Doesn'tContain3.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	cout << "enter the ending range from 1 to n" << endl;
+
+	int num;
+
+	cin >> num;
+
+	for (int i = 0; i < num; ++i)
+	{
+		/* code */
+	}
+
 	return 0;
 }
