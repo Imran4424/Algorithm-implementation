3cafcfe810adb05559707d33059ec9a35b75276a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-29 10:16:36 +0600

    mathematics

--- Mathematics/AverageOnAppearence.cpp
+++ Mathematics/AverageOnAppearence.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	cout << "enter any number" << endl;
+	cout << "enter zero to exit" << endl;
+
+	int count = 0;
+	int avg = 0;
+
+	double num;
+	cin >> num; 
+
+	while(num)
+	{
+		count++;
+
+		avg = (((count-1) * avg) + num ) / count;
+
+		cout << "Current average: " << avg << endl;
+
+		cout << "enter any number" << endl;
+		cout << "enter zero to exit" << endl;
+		
+	}
+
 	return 0;
 }
