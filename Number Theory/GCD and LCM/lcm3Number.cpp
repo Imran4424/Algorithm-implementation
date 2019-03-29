95366ee7856f76b70cb11a7736073f0603be2434
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-29 21:51:25 +0600

    mathematics

--- Number Theory/GCD and LCM/lcm3Number.cpp
+++ Number Theory/GCD and LCM/lcm3Number.cpp
@@ -51,3 +51,56 @@ int main()
 
     return 0;
 }
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+typedef long long int lli;
+
+lli GCD(lli a, lli b)
+{
+        if(a == 0 || b == 0)
+        {
+                return 0;
+        }
+
+        if(a < b)
+        {
+                swap(a, b);
+        }
+
+        if(a % b == 0)
+        {
+                return b;
+        }
+        else
+        {
+                return GCD(b, a % b);
+        }
+}
+
+
+lli LCM(lli a,lli b,lli c,lli g)
+{
+        lli l = ((a*b*c)/pow(g,2));
+
+        return l;
+}
+
+int main()
+{
+        lli a,b,c;
+
+        while(cin >> a >> b >> c)
+        {
+                lli temp = GCD(a,b);
+                ///cout<<temp<<endl;
+                lli g = (c,temp);
+                ///cout<<g<<endl;
+
+                cout<<g<<" "<<LCM(a,b,c,g)<<endl;
+        }
+
+    return 0;
+}
