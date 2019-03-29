8f77b1f216abba84bc41a2fec45045b5d5148421
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-29 22:44:12 +0600

    gcd lcm

---
+++ Number Theory/GCD and LCM/lcmMultipleNum.cpp
@@ -0,0 +1,23 @@
+
+
+lli GCD(lli a, lli b)
+{
+        if(a == 0 || b == 0)
+        {
+                return 0;
+        }
+
+        if(a < b)
+        {
+                swap(a, b);
+        }
+
+        if(a % b == 0)
+        {
+                return b;
+        }
+        else
+        {
+                return GCD(b, a % b);
+        }
+}
