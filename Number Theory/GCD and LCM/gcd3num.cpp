125cc33ca5148130de6e185fc759c08c2c16d077
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-29 19:58:39 +0600

    mathematics

--- Number Theory/GCD and LCM/gcd3num.cpp
+++ Number Theory/GCD and LCM/gcd3num.cpp
@@ -4,22 +4,28 @@ using namespace std;
 
 typedef long long int lli;
 
-lli gcd(lli a,lli b)
+lli GCD(lli a, lli b)
 {
-    if(a<b)
-    {
-        swap(a,b);
-    }
+        if(a == 0 || b == 0)
+        {
+                return 0;
+        }
 
-    if(a%b == 0)
-    {
-        return b;
-    }
-    else
-    {
-        return gcd(a,a%b);
-    }
+        if(a < b)
+        {
+                swap(a, b);
+        }
+
+        if(a % b == 0)
+        {
+                return b;
+        }
+        else
+        {
+                return GCD(b, a % b);
+        }
 }
+
 
 int main()
 {
