fa2f88910dd809d603fd456dac8fb9b8e39a0388
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-29 21:43:41 +0600

    mathematics

--- Number Theory/GCD and LCM/lcm.cpp
+++ Number Theory/GCD and LCM/lcm.cpp
@@ -5,43 +5,49 @@ using namespace std;
 
 typedef long long int lli;
 
-lli GCD(lli a,lli b)
+lli GCD(lli a, lli b)
 {
-    if(a<b)
-    {
-        swap(a,b);
-    }
+        if(a == 0 || b == 0)
+        {
+                return 0;
+        }
 
-    if(a%b == 0)
-    {
-        return b;
-    }
-    else
-    {
-        return GCD(a,a%b);
-    }
+        if(a < b)
+        {
+                swap(a, b);
+        }
+
+        if(a % b == 0)
+        {
+                return b;
+        }
+        else
+        {
+                return GCD(b, a % b);
+        }
 }
+
 
 lli LCM(lli a,lli b,lli c,lli g)
 {
-    lli l = ((a*b*c)/pow(g,2));
+        lli l = ((a*b*c)/pow(g,2));
 
-    return l;
+        return l;
 }
 
 int main()
 {
-    lli a,b,c;
+        lli a,b,c;
 
-    while(cin>>a>>b>>c)
-    {
-        lli temp = GCD(a,b);
-        ///cout<<temp<<endl;
-        lli g = (c,temp);
-        ///cout<<g<<endl;
+        while(cin >> a >> b >> c)
+        {
+                lli temp = GCD(a,b);
+                ///cout<<temp<<endl;
+                lli g = (c,temp);
+                ///cout<<g<<endl;
 
-        cout<<g<<" "<<LCM(a,b,c,g)<<endl;
-    }
+                cout<<g<<" "<<LCM(a,b,c,g)<<endl;
+        }
 
     return 0;
 }
