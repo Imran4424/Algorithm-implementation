780da6549466526536275291c7adbaee5e64a219
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-29 20:27:25 +0600

    mathematics

--- Number Theory/GCD and LCM/gcdMultipleNum.cpp
+++ Number Theory/GCD and LCM/gcdMultipleNum.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 using namespace std;
+
+
+typedef long long int lli;
+
+
+lli GCD(lli a, lli b)
+{
+        if(a == 0 || b == 0)
+        {
+                return 0;
+        }
+
+        if(a < b)
+        {
+                swap(a, b);
+        }
+
+        if(a % b == 0)
+        {
+                return b;
+        }
+        else
+        {
+                return GCD(b, a % b);
+        }
+}
