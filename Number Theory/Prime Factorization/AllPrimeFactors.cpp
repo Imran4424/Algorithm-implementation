37832cd7a5812f66601dcf26744c41469f326f8d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-30 18:09:54 +0600

    gcd lcm

--- Number Theory/Prime Factorization/AllPrimeFactors.cpp
+++ Number Theory/Prime Factorization/AllPrimeFactors.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 void primeFactors(int num)
 {
-	
+	// print the number of 2's if it divide num
+
+	while(num % 2 == 0)
+	{
+		cout << 2 << " ";
+
+		num = num / 2 ;
+	}
 }
 
 int main(int argc, char const *argv[])
