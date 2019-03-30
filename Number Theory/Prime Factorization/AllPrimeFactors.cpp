2e57636bc072615601bb3b2b2982c1f63b0c6340
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-30 18:16:16 +0600

    gcd lcm

--- Number Theory/Prime Factorization/AllPrimeFactors.cpp
+++ Number Theory/Prime Factorization/AllPrimeFactors.cpp
@@ -12,6 +12,21 @@ void primeFactors(int num)
 
 		num = num / 2 ;
 	}
+
+	for(int i = 3; i <= sqrt(num); i = i + 2)
+	{
+		while(num % i == 0)
+		{
+			cout << i << " ";
+
+			num = num / i;
+		}
+	}
+
+	if (num > 2)
+	{
+		cout << num << endl;
+	}
 }
 
 int main(int argc, char const *argv[])
