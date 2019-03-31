50be3e4c6449f7eb51700ce5fdfd1597dfbba5d2
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-31 18:58:17 +0600

    prime

--- Number Theory/Primality Test/4.Fermat.cpp
+++ Number Theory/Primality Test/4.Fermat.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 using namespace std;
 
+typedef long long int lli;
+
+bool isPrime(lli num, llli k = 25)
+{
+	if(num <= 1 || num == 4)
+	{
+		return false;
+	}
+
+	if (k <= 3)
+	{
+		return true;
+	}
+
+	// test k times
+
+
+}
+
 int main(int argc, char const *argv[])
 {
 	cout << " enter any natural number" << endl;
 
-	int num;
+	lli num;
 
 	while(cin >> num)
 	{
