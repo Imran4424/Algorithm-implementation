27f3d0bf0c4b01b505623bfb152a6ec83fe7208c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-31 18:52:33 +0600

    prime

--- Number Theory/Primality Test/4.Fermat.cpp
+++ Number Theory/Primality Test/4.Fermat.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	cout << " enter any natural number" << endl;
+
+	int num;
+
+	while(cin >> num)
+	{
+		if(isPrime(num))
+		{
+			cout << " Prime" << endl << endl;
+		}
+		else
+		{
+			cout << " Not Prime" << endl << endl;
+		}
+
+		cout << " enter any natural number" << endl;
+	}
+
 	
 	return 0;
 }
