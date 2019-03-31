6777a744e61fe745af93e62e0af65cc3932e340f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-31 17:29:31 +0600

    prime factorization

--- Number Theory/Primality Test/1.PrimalityTest.cpp
+++ Number Theory/Primality Test/1.PrimalityTest.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+bool isPrime(int num)
+{
+	if(n <= 1)
+	{
+		return false;
+	}
+
+	for(int i=2; i <= sqrt(num); ++i)
+	{
+		if(num % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
 
 int main(int argc, char const *argv[])
 {
