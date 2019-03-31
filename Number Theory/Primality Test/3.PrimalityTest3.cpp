e36c11dbf93ef91d6d68a2506715e8c82e0b6184
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-31 17:47:02 +0600

    prime

--- Number Theory/Primality Test/3.PrimalityTest3.cpp
+++ Number Theory/Primality Test/3.PrimalityTest3.cpp
@@ -13,14 +13,27 @@ bool isPrime(int num)
 		return true
 	}
 
-	if(num % 2 == 0 || num % 3 == 0)
+	/*
+		checking (6k + i)
+
+		here, i = -1, 0, 1, 2, 3, 4
+
+		2 divides (6k + 0), (6k + 2), (6k + 4)
+		3 divides (6k + 3)
+	*/
+
+	if(num % 2 == 0 || num % 3 == 0) 
 	{
 		return false;
 	}
 
 	for(int i = 5; i*i <= num; i = i + 6)
 	{
-		if(num % i == 0)
+		/*
+			checking (6k - 1) and (6k + 1)
+		*/
+
+		if(num % i == 0 || num % (i+2) == 0)
 		{
 			return false;
 		}
