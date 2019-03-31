489f4f6313b72e46ad688e2449f0922cd283e118
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-31 08:56:58 +0600

    prime factorization

--- Number Theory/Prime Factorization/AllDivisor.cpp
+++ Number Theory/Prime Factorization/AllDivisor.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+
+void printAllDivisor(int num)
+{
+	for (int i = 1; i <= sqrt(num); ++i)
+	{
+		/* code */
+	}
+}
 
 int main(int argc, char const *argv[])
 {
