10fce93ef468aa0160897d91a9047c1e5e7826c9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-03-31 19:56:42 +0600

    sieve

--- Number Theory/Sieve/1.SieveOfEratosthenes.cpp
+++ Number Theory/Sieve/1.SieveOfEratosthenes.cpp
@@ -7,7 +7,13 @@ void SieveOfEratosthenes(lli num)
 {
 	bool prime[num + 1];
 
-	
+	/*
+		memset sample syntax:
+
+		memset(arr_name, value, sizeof(arr_name));
+	*/
+
+	memset(prime, true, sizeof(prime));
 }
 
 int main(int argc, char const *argv[])
