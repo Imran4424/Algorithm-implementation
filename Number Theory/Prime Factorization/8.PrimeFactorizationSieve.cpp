e744db4daae463f738a21e6ff20f5772aa378e76
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-04-01 08:57:49 +0600

    sieve

--- Number Theory/Prime Factorization/8.PrimeFactorizationSieve.cpp
+++ Number Theory/Prime Factorization/8.PrimeFactorizationSieve.cpp
@@ -14,7 +14,25 @@ void Sieve()
 
 	mark[0] = mark [1] = false;
 
-	
+	for(lli p = 2; p*p <= limit; p++)
+	{
+		if (mark[p] == true)
+		{
+			for(lli i = p*p; i <= limit; i = i+p)
+			{
+				mark[i] = false;
+			}
+		}
+	}
+
+
+	for (int i = 2; i <= num; ++i)
+	{
+		if (mark[i])
+		{
+			prime.push_back(i);
+		}
+	}
 }
 
 int main(int argc, char const *argv[])
