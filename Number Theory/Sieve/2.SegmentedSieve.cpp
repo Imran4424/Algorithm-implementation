33bd3d7266a42f0cc6f06c976a064bd69cc0f5a8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-04-01 07:11:10 +0600

    sieve

--- Number Theory/Sieve/2.SegmentedSieve.cpp
+++ Number Theory/Sieve/2.SegmentedSieve.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 using namespace std;
 
+typedef long long int lli;
+
+void SieveOfEratosthenes(lli limit, vector <int> &prime)
+{
+	bool mark[num + 1];
+
+	/*
+		memset sample syntax:
+
+		memset(arr_name, value, sizeof(arr_name));
+	*/
+
+	memset(mark, true, sizeof(mark));
+
+	mark[0] = mark[1] = false;
+
+	for(lli p = 2; p*p <= num; p++)
+	{
+		if (prime[p] == true)
+		{
+			for(lli i = p*p; i <= num; i = i+p)
+			{
+				prime[i] = false;
+			}
+		}
+	}
+
+
+	for (int i = 2; i <= num; ++i)
+	{
+		if (prime[i])
+		{
+			cout << i << " ";
+		}
+	}
+
+	cout << endl << endl;
+}
 
 int main(int argc, char const *argv[])
 {
