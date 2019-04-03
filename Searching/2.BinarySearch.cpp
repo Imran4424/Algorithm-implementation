f77aa7f1439a73c54343aed0427ff51685f90310
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-04-03 22:52:24 +0600

    search

--- Searching/2.BinarySearch.cpp
+++ Searching/2.BinarySearch.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
-int BinarySearch(vector<int> ar)
+int BinarySearch(vector<int> ar, int target)
 {
 
 }
