2de5f7aca99865091271dd1e351682a48a3f5c7f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-04-03 20:50:00 +0600

    searching

--- Searching/LinearSearh.cpp
+++ Searching/LinearSearh.cpp
@@ -4,19 +4,36 @@ using namespace std;
 
 int LinearSearch(vector<int> ar)
 {
-
+	for (int i = 0; i < ar.size(); ++i)
+	{
+		/* code */
+	}
 }
 
 int main(int argc, char const *argv[])
 {
 	cout << "enter the size of the array" << endl;
 
-	int size;
-	cin >> size;
+	int n;
+	cin >> n;
 
-	vector <int> numbers(size);
+	vector <int> numbers(n);
 
-	
+	for (int i = 0; i < numbers.size(); ++i)
+	{
+		int temp = rand() % 100;
+
+		temp = (temp + i - 50);
+
+		if (temp < 0)
+		{
+			temp = -temp;
+		}
+
+		numbers.push_back(temp);
+	}
+
+	int position = LinearSearch(numbers);
 
 	return 0;
 }
