5a087c3a8b55afd229da3004a790dcde37407ef9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-04-04 05:45:41 +0600

    search

--- Searching/2.BinarySearch.cpp
+++ Searching/2.BinarySearch.cpp
@@ -9,3 +9,43 @@ int BinarySearch(vector<int> ar, int target)
 {
 
 }
+
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the size of the array" << endl;
+
+	int n;
+	cin >> n;
+
+	vector <int> numbers(n);
+
+	srand(time(0)); // for managing session
+
+	for (int i = 0; i < numbers.size(); ++i)
+	{
+		int temp = rand() % 100;
+
+		
+
+		numbers[i] = temp + 1;
+	}
+
+	cout << "enter your target" << endl;
+
+	int target;
+	cin >> target;
+
+	int position = LinearSearch(numbers, target);
+
+	if (position == -1)
+	{
+		cout << "target does not exist in the array" << endl;
+	}
+	else
+	{
+		cout << "found in position: " << position << endl;
+	}
+
+	return 0;
+}
