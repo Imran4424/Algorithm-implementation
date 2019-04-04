d3b9276407deba8233c8c4c8394a4126fe898319
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-04-04 07:45:21 +0600

    search

--- Searching/test.cpp
+++ Searching/test.cpp
@@ -30,14 +30,14 @@ int BinarySearch(vector<int> ar, int target, int start, int stop)
 		{
 			return mid + 1;
 		}
-		else if (ar[mid] > target)
+
+		if (ar[mid] > target)
 		{
 			return BinarySearch(ar, target, start, mid-1);
 		}
-		else if (ar[mid] < target)
-		{
-			return BinarySearch(ar, target, mid+1, stop);
-		}
+		
+
+		return BinarySearch(ar, target, mid+1, stop);
 		
 	}
 
