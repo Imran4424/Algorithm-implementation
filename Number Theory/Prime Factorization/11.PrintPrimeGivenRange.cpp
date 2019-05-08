0ea6ddc3c0b60bc777c5bfbe234ba8a8fcf9d693
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-08 13:51:51 +0600

    number theory

--- Number Theory/Prime Factorization/11.PrintPrimeGivenRange.cpp
+++ Number Theory/Prime Factorization/11.PrintPrimeGivenRange.cpp
@@ -2,6 +2,17 @@
 #include <vector>
 using namespace std;
 
+typedef long long int lli;
+
+vector<bool> Sieve(int num)
+{
+	vector<bool> prime(num+1, true);
+
+	prime[0] = prime[1] = false;
+
+	
+}
+
 int main(int argc, char const *argv[])
 {
 	
