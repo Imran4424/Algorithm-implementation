f13003aabd685fc414950c7370da8d90aa8dc77a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-08 15:16:41 +0600

    number theory

--- Number Theory/Prime Factorization/12.KthPrimeFactor.cpp
+++ Number Theory/Prime Factorization/12.KthPrimeFactor.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 vector<int> AllPrimeFactor(int num)
 {
-	
+	vector <int> primeFactors;
+
+	while(num % 2 == 0)
+	{
+		primeFactors.push_back(2);
+
+		num /= 2;
+	}
+
+	for(int i=3; i*i <= num; i = i+2)
+	{
+		while(num % i == 0)
+		{
+			primeFactors.push_back(i);
+
+			num /= i;
+		}
+	}
 }
 
 
