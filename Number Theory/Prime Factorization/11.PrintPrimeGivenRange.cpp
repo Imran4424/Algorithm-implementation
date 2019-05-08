44303e091e577d2c494169eb6a8e85c250478a3e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-08 13:59:11 +0600

    number theory

--- Number Theory/Prime Factorization/11.PrintPrimeGivenRange.cpp
+++ Number Theory/Prime Factorization/11.PrintPrimeGivenRange.cpp
@@ -10,7 +10,33 @@ vector<bool> Sieve(int num)
 
 	prime[0] = prime[1] = false;
 
-	
+	for(int i=2; i*i <= num; i++)
+	{
+		if(prime[i] == true)
+		{
+			for(int j=i*i; j<=num; j = j+i)
+			{
+				prime[j] = false;
+			}
+		}
+	}
+
+	return prime;
+}
+
+void PrintPrime(int start, int stop)
+{
+	vector <bool> prime = Sieve(stop);
+
+	for(int i=start; i<=stop; i++)
+	{
+		if(prime[i])
+		{
+			cout << prime[i] << " ";
+		}
+	}
+
+	cout << endl;
 }
 
 int main(int argc, char const *argv[])
