f644cb28ba9c0aa5f6caab10b4747f013f668bb7
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-12 17:52:14 +0600

    backtracking

--- Backtracking/Knights Tour/Backtrack.c
+++ Backtracking/Knights Tour/Backtrack.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+bool isVisited[8][8];
+int solution[8][8];
+
+bool isSafe(int row, int col)
+{
+	if(row >= 0 && row < 8)
+	{
+		if (col >= 0 && col < 8)
+		{
+			if (!isVisited[row][col])
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+
+bool KnightTour(int row, int col, int noMoves)
+{
+	if (noMoves == 8*8)
+	{
+		return true;
+	}
+}
+
 
 int main(int argc, char const *argv[])
 {
+	for (int i = 0; i < 8; ++i)
+	{
+		for(int j = 0; j < 8; ++j)
+		{
+			isVisited[i][j] = false;
+		}
+	}
 	
 	return 0;
 }
