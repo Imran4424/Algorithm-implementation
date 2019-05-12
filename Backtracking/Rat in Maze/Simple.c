067a1258a47656c6b7990d5e284b2091757dae84
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-12 15:31:59 +0600

    backtracking

--- Backtracking/Rat in Maze/Simple.c
+++ Backtracking/Rat in Maze/Simple.c
@@ -4,9 +4,35 @@ int maze[40][40];
 
 int path[40][40];
 
+int maxRow, maxCol;
+
+
 
 int main(int argc, char const *argv[])
 {
-	
+	printf("enter row and col of the maze\n");
+
+	scanf("%d %d", &row, &col);
+
+	printf("enter the maze matrix\n");
+
+	for(int i=0; i < row; i++)
+	{
+		for(int col=0; j < col; j++)
+		{
+			scanf("%d", &maze[i][j]);
+		}
+	}
+
+
+	for(int i=0; i < row; i++)
+	{
+		for(int col=0; j < col; j++)
+		{
+			path = 0;
+		}
+	}
+
+
 	return 0;
 }
