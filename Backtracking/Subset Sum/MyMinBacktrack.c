1a5fb2d43be9c74b0b6f76e7bac94ec508d744a9
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-13 21:15:36 +0600

    backtracking

--- Backtracking/Subset Sum/MyMinBacktrack.c
+++ Backtracking/Subset Sum/MyMinBacktrack.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int partitioning(int ar[], int start, int end)
+{
+
+	int pivot = ar[end];
+
+	int pIndex = start;
+
+	for (int i = start; i < end; ++i)
+	{
+		if (ar[i] <= pivot)
+		{
+			int temp = ar[i];
+			ar[i] = ar[pIndex];
+			ar[pIndex] = temp;
+
+			pIndex++;
+		}
+	}
+
+	int temp = ar[end];
+	ar[end] = ar[pIndex];
+	ar[pIndex] = temp;
+
+
+	return pIndex;
+}
+
+
+void quick_sort(int ar[], int start,int end)
+{
+	if (start < end)
+	{
+		int pIndex = partitioning(ar,start,end);
+
+		quick_sort(ar,start,pIndex-1);
+		quick_sort(ar,pIndex+1,end);
+	}
+}
+
+
+
 
 int main(int argc, char const *argv[])
 {
