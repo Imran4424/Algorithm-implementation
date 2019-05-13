8fdb965b380e2dec8c43374a09502b7959cccfed
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-13 13:28:33 +0600

    backtracking

--- Backtracking/Subset Sum/Naive.cpp
+++ Backtracking/Subset Sum/Naive.cpp
@@ -3,9 +3,38 @@
 #include <algorithm>
 using namespace std;
 
+void SubSetSum(vector<int> input, int target)
+{
+	sort(input.begin(), input.end());
+
+	int i=0;
+
+	while(input[i] <= target)
+	{
+		i++;
+	}
+
+	input.resize(i);
+
+}
 
 int main(int argc, char const *argv[])
 {
-	
+	vector <int> input;
+
+	int size, target;
+	cin >> size >> target;
+
+	for(int i=0; i< size; ++i)
+	{
+		int temp;
+		cin >> temp;
+
+		input.push_back(temp);
+	}
+
+
+
+
 	return 0;
 }
