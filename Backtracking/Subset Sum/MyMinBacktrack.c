83e28ee3e6cb6d2eb7b6f4fcd4b696dc0eca5854
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-13 21:09:46 +0600

    backtracking

--- Backtracking/Subset Sum/MyMinBacktrack.c
+++ Backtracking/Subset Sum/MyMinBacktrack.c
@@ -4,6 +4,27 @@
 
 int main(int argc, char const *argv[])
 {
+		int size, target;
+
+	scanf("%d %d", &size, &target);
+
+	int input[size];
+
+	for (int i = 0; i < size; ++i)
+	{
+		scanf("%d", &input[i]);
+	}
+
+	bool status = SubsetSum(input, size, target);
+
+	if(status)
+	{
+		printf("Solution exists\n");
+	}
+	else
+	{
+		printf("Solution doesn't exists\n");
+	}
 	
 	return 0;
 }
