7a979d27ff3c8aa878f3025e0966574757e70ba1
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-13 15:32:00 +0600

    backtracking

--- Backtracking/Subset Sum/Naive.c
+++ Backtracking/Subset Sum/Naive.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool SubsetSum()
+bool SubsetSum(int input[], int size, int sum, int target)
 {
-	
+
 }
 
 int main(int argc, char const *argv[])
 {
+	int size, target;
+
+	scanf("%d %d", &size, &target)
+
+	int input[size];
+
+	for (int i = 0; i < size; ++i)
+	{
+		scanf("%d", &input[i]);
+	}
+
+
 	
 	return 0;
 }
