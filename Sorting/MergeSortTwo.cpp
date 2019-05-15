0fe8649378c693c2eb7af4ffc94aae13361d7318
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-15 10:47:10 +0600

    backtracking

--- Sorting/MergeSortTwo.cpp
+++ Sorting/MergeSortTwo.cpp
@@ -3,6 +3,42 @@
 using namespace std;
 
 
+vector <int> Merge(vector <int> left, vector <int> right, vector <int> ar)
+{
+	ar.clear();
+
+	int i=0,
+}
+
+vector <int> MergeSort(vector <int> ar)
+{
+	if(ar.size() < 2)
+	{
+		return ar;
+	}
+
+	int mid = ar.size() / 2;
+
+	vector <int> left, right;
+
+	for (int i = 0; i < mid; ++i)
+	{
+		left.push_back(ar[i]);
+	}
+
+	for (int i = mid; i < ar.size(); ++i)
+	{
+		right.push_back(ar[i]);
+	}
+
+	left  = MergeSort(left);
+
+	right = MergeSort(right);
+
+	return Merge(left, right, ar);
+}
+
+
 int main(int argc, char const *argv[])
 {
 	
