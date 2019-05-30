19190570acb701b2b3eac3c2944c41da7e1e02b1
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-30 11:48:26 +0600

    backtracking

--- Backtracking/m coloring prob/SimpleBiColoring.cpp
+++ Backtracking/m coloring prob/SimpleBiColoring.cpp
@@ -37,7 +37,7 @@ int main(int argc, char const *argv[])
 
 	vector <int> currentColor(vertices+1, -1);
 
-	GraphColoring(list, currentColor, 1);
+	BiColoring(list, currentColor, 1);
 
 	return 0;
 }
