21ddd69e78db4967f4931e4c2f271a5881eee85d
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-30 09:58:52 +0600

    backtracking

--- Backtracking/m coloring prob/SimpleThreeColoring.cpp
+++ Backtracking/m coloring prob/SimpleThreeColoring.cpp
@@ -21,4 +21,28 @@ int main(int argc, char const *argv[])
 }
 
 
+/*
+	https://www.geeksforgeeks.org/m-coloring-problem-backtracking-5/
 
+	geeksforgeeks problem
+
+	Input
+
+	10 15 - vetices and edges
+
+	1 2
+	1 3
+	1 4
+	2 5
+	2 9
+	3 6
+	3 10
+	4 7
+	4 8
+	5 6
+	5 8
+	6 7
+	7 9
+	8 10
+	9 10
+*/
