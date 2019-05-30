6e5cd0f063f47d9c3a7a5eead14240ee5a07f851
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-30 09:37:06 +0600

    backtracking

--- Backtracking/m coloring prob/SimpleThreeColoring.cpp
+++ Backtracking/m coloring prob/SimpleThreeColoring.cpp
@@ -8,6 +8,12 @@ int main(int argc, char const *argv[])
 {
 	cout << "enter the number of vertices" << endl;
 
+	int vertices;
+
+	cout << "enter the adjacency node" << endl;
+
+	vector <int> list[vertices];
+
 
 	return 0;
 }
