f5f20edc915625e7ef8787379d6a4e0af16530a1
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-30 16:41:18 +0600

    backtracking

--- Backtracking/m coloring prob/SimpleBiColoring.cpp
+++ Backtracking/m coloring prob/SimpleBiColoring.cpp
@@ -10,11 +10,12 @@ bool BiColoring(vector< vector <int> > list, vector<int> currentColor, int start
 
 	currentColor[startNode] = 1;
 
-	currentList.push_back(startNode);
+	currentList.push(startNode);
 
 	while(!currentList.empty())
 	{
-		
+		int hand = currentList.front();
+		currentList.pop()
 	}
 }
 
