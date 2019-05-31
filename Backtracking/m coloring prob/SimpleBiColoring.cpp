d4fbde29de2fc7e8c9b30cb770d5fb8d6cdc7b6e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-31 09:37:59 +0600

    backtracking

--- Backtracking/m coloring prob/SimpleBiColoring.cpp
+++ Backtracking/m coloring prob/SimpleBiColoring.cpp
@@ -21,7 +21,10 @@ bool BiColoring(vector< vector <int> > list, vector<int> currentColor, int start
 		{
 			for (int i = 0; i < list[hand].size(); ++i)
 			{
-				
+				if (currentColor[])
+				{
+					/* code */
+				}
 				
 			}
 		}
