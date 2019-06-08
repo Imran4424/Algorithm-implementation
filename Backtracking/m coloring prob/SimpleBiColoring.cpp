b989033a3de06e0304cf8cd1f036dd8071a62b35
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-06-08 18:30:01 +0600

    backtracking

--- Backtracking/m coloring prob/SimpleBiColoring.cpp
+++ Backtracking/m coloring prob/SimpleBiColoring.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-bool BiColoring(vector< vector <> list, vector<int> currentColor, int startNode)
+bool BiColoring(vector< vecto> list, vector<int> currentColor, int startNode)
 {
 	queue <int> currentList;
 
