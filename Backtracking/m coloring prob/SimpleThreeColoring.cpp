f95277e9cbd9011daecd828088631d26c5223659
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-06-08 18:34:08 +0600

    backtracking

--- Backtracking/m coloring prob/SimpleThreeColoring.cpp
+++ Backtracking/m coloring prob/SimpleThreeColoring.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-int TriColoring(vector< > list, vector<int> currentColor, int startNode)
+int TriColoring(vector<> list, vector<int> currentColor, int startNode)
 {
 	queue <int> currentList;
 
