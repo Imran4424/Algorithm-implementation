d9b78c12f331172ed670166a77fd7724ee31a0b8
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-06-10 23:42:18 +0600

    divide and conquer

--- Divide Conquer/6.CountInversionsMergeSort.cpp
+++ Divide Conquer/6.CountInversionsMergeSort.cpp
@@ -78,8 +78,8 @@ int MergeSortHandler(vector <int> roksana)
 
 int main(int argc, char const *argv[])
 {
-	// vector <int> roksana{13, 1, 3, 19, 11, 3, 4, 1, 10, 8}; // 23
-	vector <int> roksana{1, 20, 6, 4, 5}; // 5
+	vector <int> roksana{13, 1, 3, 19, 11, 3, 4, 1, 10, 8}; // 23
+	// vector <int> roksana{1, 20, 6, 4, 5}; // 5
 
 	cout << "Number of inversions are: " << MergeSortHandler(roksana) << endl;
 
