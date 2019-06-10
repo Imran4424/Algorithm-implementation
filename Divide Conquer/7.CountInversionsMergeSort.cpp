4a7dd50214d6b549aa3d2f5ff16f64594c7edc22
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-06-10 17:37:38 +0600

    divide and conquer

---
+++ Divide Conquer/7.CountInversionsMergeSort.cpp
@@ -0,0 +1,35 @@
+/*
+	By using enhance merge sort
+*/
+
+#include <iostream>
+#include <vector>
+#include <time.h>
+using namespace std;
+
+int Merge()
+
+int MergeSort(vector<int> roksana, int left, int right)
+{
+	int mid, inversionCount = 0;
+
+	if (right > left)
+	{
+		mid = (left + right) / 2;
+
+		inversionCount += MergeSort(roksana, left, mid); // left sub array
+		inversionCount += MergeSort(roksana, mid+1, right); // right sub array
+
+		/* merging two parts*/
+
+
+	}
+
+	return inversionCount;
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
