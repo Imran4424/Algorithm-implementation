1918e30ce15f845f049b32aa269da0fd99531cb1
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-06-12 19:20:00 +0600

    divide and conquer

---
+++ Divide Conquer/9.PowXnNegative.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+typedef long long int lli;
+
+lli Power(lli base, lli pow)
+{
+	if (0 == pow)
+	{
+		return 1;
+	}
+
+	lli hold = Power(base, pow/2);
+
+	if (pow % 2 == 0)
+	{
+		return hold * hold;
+	}
+	else
+	{
+		return base * hold * hold;
+	}
+}
+
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the base and pow" << endl;
+
+	lli base, pow;
+	cin >> base >> pow;
+
+	cout << "Result is: " << Power(base, pow) << endl;
+
+	return 0;
+}
