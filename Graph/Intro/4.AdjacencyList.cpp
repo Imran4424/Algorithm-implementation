e7e8779a6fed1167329e35c2f07c1cf5718afee8
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-10 02:06:51 +0600

    graph

---
+++ Graph/Intro/4.AdjacencyList.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+/*
+	A utility function to add edge for directed graph
+*/
+void AddEdge(list <int> adjacency[],int u, int v) // remember this is a list array
+{
+	adjacency[u].push_back(v);
+}
+
+void Display(list <int> adjacency[], int vetex) // remember this is a list array
+{
+	for (int i = 1; i <= vetex; ++i)
+	{
+		cout << i << ": ";
+
+		list <int> :: iterator it;
+
+		for (it = adjacency[i].begin(); it != adjacency[i].end(); ++it) 
+		{
+			cout << *it << " ";
+		}
+
+		cout << endl;
+	}
+
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+	// cout << "enter the vetex number" << endl;
+	int vetex = 5;
+	// cin >> vetex;
+
+	// remember this is a vector array
+	list <int> adjacency[vetex + 1]; // cause array strats count from zero
+	// list <int> *adjacency; // cause array strats count from zero
+
+	AddEdge(adjacency, 1, 2); 
+	AddEdge(adjacency, 1, 5); 
+	AddEdge(adjacency, 2, 3); 
+	AddEdge(adjacency, 2, 4); 
+	AddEdge(adjacency, 2, 5); 
+	AddEdge(adjacency, 3, 4); 
+	AddEdge(adjacency, 4, 5); 
+
+	Display(adjacency, vetex);
+
+	return 0;
+}
