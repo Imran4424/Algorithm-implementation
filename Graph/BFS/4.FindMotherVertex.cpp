dfb41a51c812349923ad1b56d9e124bb0e94e70e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-18 09:44:40 +0600

    graph

--- Graph/BFS/4.FindMotherVertex.cpp
+++ Graph/BFS/4.FindMotherVertex.cpp
@@ -2,7 +2,9 @@
 	Mother vertex - In graph mother vertex is that vertex or node, from which we can reach all other
 	vertices in the graph
 
-	
+	In a graph there can more than one mother vertex
+
+	In this we will print just one.
 */
 #include <iostream>
 #include <vector>
