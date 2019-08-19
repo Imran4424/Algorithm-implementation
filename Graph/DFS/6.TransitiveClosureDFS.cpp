c619f8df986b29e4f73c499a404353d62aa8cf25
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-19 11:35:39 +0600

    graph

--- Graph/DFS/6.TransitiveClosureDFS.cpp
+++ Graph/DFS/6.TransitiveClosureDFS.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 vector <int> *adjacency;
 
+void AddEdge(int u, int v) // 
+{
+	adjacency[u].push_back(v);
+}
+
 int main(int argc, char const *argv[])
 {
 	cout << "enter the number of vertex" << endl;
