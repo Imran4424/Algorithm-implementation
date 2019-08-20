2e4b7639ba887db078fed3e0c94e15a07207b2f8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-20 20:59:06 +0600

    graph

--- Graph/DFS/7.FindKCores.cpp
+++ Graph/DFS/7.FindKCores.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 vector <int> *adjacency;
 
+void AddEdge(int u, int v) // unidirected graph
+{
+	AddEdge[u].push_back(v);
+
+	AddEdge[v].push_back(u);
+}
+
 
 int main(int argc, char const *argv[])
 {
