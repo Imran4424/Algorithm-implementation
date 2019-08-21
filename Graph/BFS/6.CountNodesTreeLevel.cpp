30d9ddc188587ab26a1f7b64146ac5e037681156
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-21 11:39:42 +0600

    graph

--- Graph/BFS/6.CountNodesTreeLevel.cpp
+++ Graph/BFS/6.CountNodesTreeLevel.cpp
@@ -3,11 +3,15 @@
 #include <queue>
 using namespace std;
 
+vector <int> *adjacency;
+
 int main(int argc, char const *argv[])
 {
 	cout << "enter the number of vertex" << endl;
 
 	int vertex;
 	cin >> vertex;
+	
+	adjacency = new vector <int> [vertex + 1];
 	return 0;
 }
