f89c585fce65547482ec6a94f9c177aca126f8ae
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-23 16:09:39 +0600

    dynamic programming

--- Dynamic Programming/Basic Concepts/1.FibonacciRecursion.cpp
+++ Dynamic Programming/Basic Concepts/1.FibonacciRecursion.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int Fibonacci()
+int Fibonacci(int n)
+{
+	if(n <= 1)
+	{
+		return n;
+	}
+
+	return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
 
 int main(int argc, char const *argv[])
 {
