54b38237f699d604e345fe0094594342001d4538
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-23 16:11:55 +0600

    dynamic programming

--- Dynamic Programming/Basic Concepts/1.FibonacciRecursion.cpp
+++ Dynamic Programming/Basic Concepts/1.FibonacciRecursion.cpp
@@ -13,7 +13,14 @@ int Fibonacci(int n)
 
 int main(int argc, char const *argv[])
 {
-	
+	cout << "enter the nth number" << endl;
+
+	int n;
+	cin >> n;
+
+	int result = Fibonacci(n);
+
+	cout << "The nth Fibonacci number is: " << result << endl;
 
 	return 0;
 }
