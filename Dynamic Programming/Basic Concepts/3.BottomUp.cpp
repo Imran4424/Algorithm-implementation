1d8ac031939a2e1f4149287ba3e6a57a9ec59690
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-24 11:41:21 +0600

    dynamic programming

--- Dynamic Programming/Basic Concepts/3.BottomUp.cpp
+++ Dynamic Programming/Basic Concepts/3.BottomUp.cpp
@@ -14,7 +14,12 @@ int Fibonacci(int n)
 	fib[0] = 0;
 	fib[1] = 1;
 
-	
+	for (int i = 2; i <= n; ++i)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+
+	return fib[n];
 }
 
 int main(int argc, char const *argv[])
