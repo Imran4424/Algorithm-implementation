736fd919394e88d6f9578cfbed8b6a62f4c265b0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-24 16:12:49 +0600

    dynamic programming

--- Dynamic Programming/Basic Problem/1.UglyNumbers.cpp
+++ Dynamic Programming/Basic Problem/1.UglyNumbers.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int GetNthUgly(int n)
+{
+
+}
+
 int main(int argc, char const *argv[])
 {
 	cout << "enter the nth term" << endl;
+
+	int n;
+	cin >> n;
+
+	GetNthUgly(n);
 
 	return 0;
 }
