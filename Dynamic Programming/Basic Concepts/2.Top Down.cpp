8d5b0afa60aaac41ef58f5d2610086443871190b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-24 10:43:15 +0600

    dynamic programming

--- Dynamic Programming/Basic Concepts/2.Top Down.cpp
+++ Dynamic Programming/Basic Concepts/2.Top Down.cpp
@@ -4,3 +4,17 @@
 
 #include <iostream>
 using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the nth number" << endl;
+
+	int n;
+	cin >> n;
+
+	int result = Fibonacci(n);
+
+	cout << "The nth Fibonacci number is: " << result << endl;
+	
+	return 0;
+}
