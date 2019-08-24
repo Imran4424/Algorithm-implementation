958b2c111766c1089fca19abd79560f6148246e9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-24 18:15:12 +0600

    dynamic programming

--- Dynamic Programming/Basic Problem/1.UglyNumbersBottomUp.cpp
+++ Dynamic Programming/Basic Problem/1.UglyNumbersBottomUp.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 typedef long long int lli;
 
+void GetNthUgly(lli n)
+{
+	lli ugly[n];
+	lli i2 = 0, i3 = 0, i5 = 0;
+
+	lli nexMultipleOf2 = 2;
+	lli nexMultipleOf3 = 3;
+	lli nexMultipleOf5 = 5;
+}
+
 int main(int argc, char const *argv[])
 {
-	
+	cout << "enter the nth term" << endl;
+
+	lli n;
+	cin >> n;
+
+	GetNthUgly(n);
+
 	return 0;
 }
