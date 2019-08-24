7a7390e7ed8b60920959cded38419ffcf8a4f0e3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-24 16:28:01 +0600

    dynamic programming

--- Dynamic Programming/Basic Problem/1.UglyNumbers.cpp
+++ Dynamic Programming/Basic Problem/1.UglyNumbers.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int MaxDivide(int x, int y)
+{
+	while(x % y == 0)
+	{
+		x = x / y; 
+	}
+
+	return x;
+}
+
 bool IsUgly(int num)
 {
+	num = MaxDivide(num, 2);
+	num = MaxDivide(num, 3);
+	num = MaxDivide(num, 5);
 
+	if (1 == num)
+	{
+		return true;
+	}
+
+	return false;
 }
 
 void GetNthUgly(int n)
