7efc3108bc00a82287821e903ba46c1c6f23c149
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-26 20:07:42 +0600

    dynamic programming

--- Dynamic Programming/Basic Problem/4.BellNumber.cpp
+++ Dynamic Programming/Basic Problem/4.BellNumber.cpp
@@ -1,5 +1,8 @@
 /*
 	Bell Number is a number that determines how many partitions we can do in a set
-	
+
+	In this code, we will implement bell triangle to find bell number series
+
+	https://en.wikipedia.org/wiki/Bell_number
 */ 
 
