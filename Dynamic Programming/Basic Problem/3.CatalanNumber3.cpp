b710fe85353130cf9fa491dc7b64e821e2bb60d0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-26 12:53:46 +0600

    dynamic programming

--- Dynamic Programming/Basic Problem/3.CatalanNumber3.cpp
+++ Dynamic Programming/Basic Problem/3.CatalanNumber3.cpp
@@ -1,8 +1,43 @@
+/*
+	This is a recursive solution of catalan number
+
+	this code implements the recursive euqation of catalan number
+
+	Cn+1 = Ci*Cn-i
+
+	adaption will be to Cn
+
+	Cn = <Summation of <0 to n-1>> Ci*Cn-i-1
+*/
 #include <iostream>
 using namespace std;
 
+typedef unsigned long long int ulli
+
+ulli Catalan(ulli num)
+{
+	if (1 >= num)
+	{
+		return 1;
+	}
+
+
+}
+
 int main(int argc, char const *argv[])
 {
+
+	cout << "enter the nth term" << endl;
+
+	ulli n;
+	cin >> n;
+
+	for (ulli i = 0; i < n; ++i)
+	{
+		Catalan(i);
+	}
+
+	cout << endl;
 	
 	return 0;
 }
