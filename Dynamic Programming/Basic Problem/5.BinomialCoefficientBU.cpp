d5bebed4322c8798b0f4866f7fff3e24cf3d45c3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-27 14:11:02 +0600

    dynamic programming

--- Dynamic Programming/Basic Problem/5.BinomialCoefficientBU.cpp
+++ Dynamic Programming/Basic Problem/5.BinomialCoefficientBU.cpp
@@ -15,3 +15,21 @@
 using namespace std;
 
 typedef long long int lli;
+
+int main(int argc, char const *argv[])
+{
+	int test = 20;
+
+	while(test--)
+	{
+		cout << "enter the n k" << endl;
+		
+		lli n, k;
+		cin >> n >> k;
+
+		cout << "The result of " << n << "C" << k << " is: " << BinomialCoefficient(n, k) << endl;
+
+		cout << endl;
+	}
+	return 0;
+}
