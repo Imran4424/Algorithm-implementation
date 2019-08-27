bc176e324fca61cddeee27e2851211994e5a78e9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-27 10:16:54 +0600

    dynamic programming

--- Dynamic Programming/Basic Problem/4.BellNumber.cpp
+++ Dynamic Programming/Basic Problem/4.BellNumber.cpp
@@ -5,4 +5,11 @@
 
 	https://en.wikipedia.org/wiki/Bell_number
 */ 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef unsigned long long int ulli;
+
+vector< vector <ulli> > belTriangle(101, vector <ulli> (101, -1));
 
