b9e567819cd8ca07cd8b4a8302aaa8cb2f5b89bc
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-27 10:14:11 +0600

    dynamic programming

--- Dynamic Programming/Basic Problem/4.BellNumberBU2.cpp
+++ Dynamic Programming/Basic Problem/4.BellNumberBU2.cpp
@@ -10,6 +10,8 @@
 	in this code, we are doing bottom up approach
 
 	can correctly calculated upto 27
+
+	more efficient
 */
 
 #include <iostream>
@@ -18,16 +20,20 @@ using namespace std;
 
 typedef unsigned long long int ulli;
 
-// vector< vector <ulli> > belTriangle(101, vector <ulli> (101, -1));
+vector< vector <ulli> > belTriangle(101, vector <ulli> (101, -1));
 
 ulli BellNumber(ulli number)
 {
-	ulli belTriangle[number+1][number+1];
 
 	belTriangle[0][0] = 1;
 
 	for (ulli i = 1; i <= number; ++i)
 	{
+		if (belTriangle[i][0] != -1)
+		{
+			continue;
+		}
+
 		// filling for j = 0 means by copying from previous row's last element
 		belTriangle[i][0] = belTriangle[i-1][i-1]; // both are i-1 one because every row same number of 
 		                                            //  column the row number
