db63aaefd80eb69dded11d4faa856e769cca806e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-27 17:57:50 +0600

    dynamic programming

---
+++ Dynamic Programming/Basic Problem/6.PermutationCoefficient2.cpp
@@ -0,0 +1,61 @@
+/*
+	In this code, we will implement permutation coefficient. This also known as permutation
+
+	we know permutation rule for nPk is
+
+	nPk = n! / (n-k)!
+
+	recursive rule of permutaion is,
+
+	P(n, k) = P(n-1, k) + k* P(n-1, k-1)
+
+	P(n, 0) = 1
+	P(n, 1) = n
+	P(n, n) = n!
+*/
+
+/*
+	In this code we will implement a old school method iterative
+*/
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+typedef long long int lli;
+
+const int size = 101;
+
+vector < vector <lli> > dpPermutations(size, vector <lli> (size, 0));
+
+
+lli PermutationCoefficient(lli number, lli combinations)
+{
+	lli result = 1;
+
+	for(lli x = number; x > number - combinations; x--)
+	{
+		result = result * x;
+	}
+
+	return result;
+}
+
+int main(int argc, char const *argv[])
+{
+	int test = 20;
+
+	while(test--)
+	{
+		cout << "enter the n k" << endl;
+		
+		lli n, k;
+		cin >> n >> k;
+
+		cout << "The result of " << n << "C" << k << " is: " << PermutationCoefficient(n, k) << endl;
+
+		cout << endl;
+	}
+	return 0;
+}
