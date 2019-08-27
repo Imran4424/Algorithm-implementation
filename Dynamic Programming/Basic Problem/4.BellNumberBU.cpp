b1eda6e842b1c0acf7c5cd0b4355e9649086b911
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-27 08:30:32 +0600

    dynamic programming

---
+++ Dynamic Programming/Basic Problem/4.BellNumberBU.cpp
@@ -0,0 +1,20 @@
+/*
+	Bell Number is a number that determines how many partitions we can do in a set
+
+	In this code, we will implement bell triangle to find bell number series
+
+	https://en.wikipedia.org/wiki/Bell_number
+*/ 
+
+#include <iostream>
+using namespace std;
+
+typedef long long int lli;
+
+vector< vector <lli> > belltriangle(101, vector <lli> (101, -1));
+
+lli BellNumber(lli number)
+{
+
+}
+
