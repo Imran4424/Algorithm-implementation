21cef1d2401ef087f333ea969ff4626e04811b9b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-27 14:49:16 +0600

    dynamic programming

---
+++ Dynamic Programming/Basic Problem/6.PermutationCoefficient.cpp
@@ -0,0 +1,12 @@
+/*
+	In this code, we will implement binomial coefficient. This also known as combination
+
+	we know combination rule for nCk is
+
+	nCk = n! / (k!*(n-k)!)
+*/
+
+/*
+	In this code we will implement Top Down approach
+*/
+
