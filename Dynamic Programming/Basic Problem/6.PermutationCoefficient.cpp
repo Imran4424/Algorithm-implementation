ee95b460be9d228c668591ea39937cf18f5c2452
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-27 14:52:22 +0600

    dynamic programming

--- Dynamic Programming/Basic Problem/6.PermutationCoefficient.cpp
+++ Dynamic Programming/Basic Problem/6.PermutationCoefficient.cpp
@@ -10,3 +10,13 @@
 	In this code we will implement Top Down approach
 */
 
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+typedef long long int lli;
+
+const int size = 101;
+
+vector < vector <lli> > dpCombinations(size, vector <lli> (size, -1));
