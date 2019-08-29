0a42e99b7f802dfbede03b6fd2d47876a5ff2fd7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-29 08:12:26 +0600

    graph

--- Graph/DFS/12.CopuntPathsAB.cpp
+++ Graph/DFS/12.CopuntPathsAB.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 vector <int> *adjacency;
 
+int count;
+
 void AddEdge(int u, int v) // directed graph
 {
 	adjacency[u].push_back(v);
@@ -11,6 +13,8 @@ void AddEdge(int u, int v) // directed graph
 
 int main(int argc, char const *argv[])
 {
+	count = 0; // clearing for every test cases
+
 	cout << "enter the number of vertex" << endl;
 
 	int vertex;
@@ -19,6 +23,22 @@ int main(int argc, char const *argv[])
 	adjacency = new vector <int> [vertex + 1]; // array of vector
 
 	cout << "How many edges?" << endl;
+
+	in edges;
+	cin >> edges;
+
+	cout << "enter the edges" << endl;
+
+	int u, v;
+
+	for (int i = 0; i < edges; ++i)
+	{
+		cin >> u >> v;
+
+		AddEdge(u, v);
+	}
+
+
 	
 	return 0;
 }
