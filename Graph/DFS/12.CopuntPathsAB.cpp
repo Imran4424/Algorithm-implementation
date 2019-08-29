f2796b72f84ea06d9496f1acefa108e84ac23706
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-29 08:02:31 +0600

    graph

--- Graph/DFS/12.CopuntPathsAB.cpp
+++ Graph/DFS/12.CopuntPathsAB.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 vector <int> *adjacency;
 
-void AddEdge(int u, int v)
+void AddEdge(int u, int v) // directed graph
 {
 	adjacency[u].push_back(v);
 }
 
 int main(int argc, char const *argv[])
 {
+	cout << "enter the number of vertex" << endl;
 	
 	return 0;
 }
