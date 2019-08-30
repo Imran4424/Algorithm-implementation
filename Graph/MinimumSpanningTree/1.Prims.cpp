51c58e4986f007650c699c34886ad083ebd5955d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-08-30 16:19:54 +0600

    graph

--- Graph/MinimumSpanningTree/1.Prims.cpp
+++ Graph/MinimumSpanningTree/1.Prims.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <pair>
 #include <queue>
 #include <algorithm>
 using namespace std;
 
-
+typedef pair <int, int> couple;
+typedef pair <int, pair <int, int> > nestedCouple;
 
 int main(int argc, char const *argv[])
 {
