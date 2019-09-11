7990189e6c0227cfd1363bf0fd026e46e5a89305
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-11 12:04:22 +0600

    dynamic

--- Dynamic Programming/Intermediate Problem/26.TravellingSalesmanNaive.cpp
+++ Dynamic Programming/Intermediate Problem/26.TravellingSalesmanNaive.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 typedef pair <int, int> couple;
 
+vector <couple> *adjacency;
+
 int main(int argc, char const *argv[])
 {
-	
+	int vertex;
+	cin >> vertex;
+
+	adjacency = new vector <couple> [vertex + 1];
+
 	return 0;
 }
