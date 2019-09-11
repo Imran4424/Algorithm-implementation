b5a660a80fcdb1051623a064e5d0ba6cb4de86d1
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-11 12:11:14 +0600

    dynamic

--- Dynamic Programming/Intermediate Problem/26.TravellingSalesmanNaive.cpp
+++ Dynamic Programming/Intermediate Problem/26.TravellingSalesmanNaive.cpp
@@ -6,12 +6,26 @@ typedef pair <int, int> couple;
 
 vector <couple> *adjacency;
 
+void AddEdge(int source, int destination, int weight)
+{
+	adjacency[source].push_back(make_pair(destination, weight));
+	adjacency[destination].push_back(make_pair(source, weight));
+}
+
 int main(int argc, char const *argv[])
 {
+	cout << "enter the vertex number" << endl;
+
 	int vertex;
 	cin >> vertex;
 
 	adjacency = new vector <couple> [vertex + 1];
+
+	cout << "how many edges ?" <<endl;
+	int edges;
+	cin >> edges;
+
+	
 
 	return 0;
 }
