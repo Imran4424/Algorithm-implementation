869e95957d37fca913899b0271d05066798d1db8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-14 20:33:27 +0600

    greedy

---
+++ Greedy/Standard Greedy/1.ActivitySelection.cpp
@@ -0,0 +1,18 @@
+/*
+	https://www.geeksforgeeks.org/activity-selection-problem-greedy-algo-1/
+
+	You are given n activities with their start and finish times. Select 
+	the maximum number of activities that can be performed by a single person, 
+	assuming that a person can only work on a single activity at a time.
+
+	
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
