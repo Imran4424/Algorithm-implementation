6434fbe4c0cd3def9754497b69f64f64cbfbdec9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-15 17:16:57 +0600

    greedy

--- Greedy/Standard Greedy/2.EgyptianFraction.cpp
+++ Greedy/Standard Greedy/2.EgyptianFraction.cpp
@@ -6,6 +6,9 @@
 
 	Following are few examples:
 
-	
+	Egyptian Fraction Representation of 2/3 is 1/2 + 1/6
+	Egyptian Fraction Representation of 6/14 is 1/3 + 1/11 + 1/231
+	Egyptian Fraction Representation of 12/13 is 1/2 + 1/3 + 1/12 + 1/156
 
+	
 */
