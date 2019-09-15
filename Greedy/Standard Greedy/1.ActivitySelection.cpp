c3468044b027f84dc374b24df82a9343e724f857
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-15 08:34:09 +0600

    greedy

--- Greedy/Standard Greedy/1.ActivitySelection.cpp
+++ Greedy/Standard Greedy/1.ActivitySelection.cpp
@@ -2,10 +2,10 @@
 	https://www.geeksforgeeks.org/activity-selection-problem-greedy-algo-1/
 
 	You are given n activities with their start and finish times. Select 
-	the maximum number of activities that can be performed by a single person, 
+	the maximum worksber of activities that can be performed by a single person, 
 	assuming that a person can only work on a single activity at a time.
 
-	Example 1 : Consider the following 3 activities sorted by start and
+	Example 1 : Consider the following 3 activities sorted by 
 	by finish time.
 
 	     start[]  =  {10, 12, 20};
@@ -16,7 +16,7 @@
 	is {0, 2} [ These are indexes in start[] and 
 	finish[] ]
 
-	Example 2 : Consider the following 6 activities sorted by start and 
+	Example 2 : Consider the following 6 activities sorted by
 	finish time.
 
 	     start[]  =  {1, 3, 0, 5, 8, 5};
@@ -33,6 +33,20 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	cout << "how many works" << endl;
+
+	int works;
+	cin >> works;
+
+	vector <int> startTime(works);
+	vector <int> finishTime(works);
+
+	cout << "enter the work's start and finish time " << endl;
+
+	for (int i = 0; i < works; ++i)
+	{
+		cin >> startTime[i] >> finishTime[i];
+	}
+
 	return 0;
 }
