17c9510a068a75d102dfaf66de53e054540df2ea
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-15 17:16:12 +0600

    greedy

--- Greedy/Standard Greedy/2.EgyptianFraction.cpp
+++ Greedy/Standard Greedy/2.EgyptianFraction.cpp
@@ -1,4 +1,11 @@
 /*
+	Every positive fraction can be represented as sum of unique unit fractions. 
+	A fraction is unit fraction if numerator is 1 and denominator is a positive 
+	integer, for example 1/3 is a unit fraction. Such a representation is called 
+	Egyptian Fraction as it was used by ancient Egyptians.
 
+	Following are few examples:
+
+	
 
 */
