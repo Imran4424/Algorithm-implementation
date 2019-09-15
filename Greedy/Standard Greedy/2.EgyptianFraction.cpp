dcefabcedf6627f141d43cf6ddbbc36b7b8b3d7f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-15 19:58:09 +0600

    greedy

--- Greedy/Standard Greedy/2.EgyptianFraction.cpp
+++ Greedy/Standard Greedy/2.EgyptianFraction.cpp
@@ -36,8 +36,8 @@ void PrintEgyptian(int numerator, int denominator)
 		return;
 	}
 
-	when denominator equally divides numerator
-	then it is not a fraction number
+	// when denominator equally divides numerator
+	// then it is not a fraction number
 
 	if (numerator % denominator == 0)
 	{
