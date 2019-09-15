3d9a1af38f9c30642c42b35040892404779a010e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-15 07:48:43 +0600

    greedy

--- Greedy/Standard Greedy/1.ActivitySelection.cpp
+++ Greedy/Standard Greedy/1.ActivitySelection.cpp
@@ -5,7 +5,27 @@
 	the maximum number of activities that can be performed by a single person, 
 	assuming that a person can only work on a single activity at a time.
 
+	Example 1 : Consider the following 3 activities sorted by
+	by finish time.
+
+	     start[]  =  {10, 12, 20};
+	     finish[] =  {20, 25, 30};
+
+	A person can perform at most two activities. The 
+	maximum set of activities that can be executed 
+	is {0, 2} [ These are indexes in start[] and 
+	finish[] ]
+
+	Example 2 : Consider the following 6 activities 
+	sorted by by finish time.
 	
+	     start[]  =  {1, 3, 0, 5, 8, 5};
+	     finish[] =  {2, 4, 6, 7, 9, 9};
+
+	A person can perform at most four activities. The 
+	maximum set of activities that can be executed 
+	is {0, 1, 3, 4} [ These are indexes in start[] and 
+	finish[] ]
 */
 
 #include <iostream>
