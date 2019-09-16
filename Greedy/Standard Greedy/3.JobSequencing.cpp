60a99928cbcb00331975cb6986f57f36faac4551
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-16 10:49:31 +0600

    greedy

--- Greedy/Standard Greedy/3.JobSequencing.cpp
+++ Greedy/Standard Greedy/3.JobSequencing.cpp
@@ -4,5 +4,30 @@
 	takes single unit of time, so the minimum possible deadline for any job is 1. 
 	How to maximize total profit if only one job can be scheduled at a time.
 
-	
+	Examples:
+
+	Input: Four Jobs with following 
+	deadlines and profits
+	JobID  Deadline  Profit
+	  a      4        20   
+	  b      1        10
+	  c      1        40  
+	  d      1        30
+
+	Output: Following is maximum profit sequence of jobs
+	        c, a   
+
+
+	Input:  Five Jobs with following
+	deadlines and profits
+	JobID   Deadline  Profit
+	  a       2        100
+	  b       1        19
+	  c       2        27
+	  d       1        25
+	  e       3        15
+
+	Output: Following is maximum profit sequence of jobs
+	        c, a, e
+
 */
