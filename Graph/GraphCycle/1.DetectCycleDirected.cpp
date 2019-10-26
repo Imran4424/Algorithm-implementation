009868901dca3416a08746bf3e5bf76bf667b244
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-26 19:23:01 +0600

    graph cycle

--- Graph/GraphCycle/1.DetectCycleDirected.cpp
+++ Graph/GraphCycle/1.DetectCycleDirected.cpp
@@ -6,10 +6,31 @@
 #include <stdio.h>
 using namespace std;
 
-const int vSize = 
+const int vSize = 50;
+
+int path[vSize][vSize];
+
+int vertex;
+
+void ReadCase()
+{
+	cin >> vertex;
+
+	int edges, source, destination;
+
+	cin >> edges;
+
+	while(edges--)
+	{
+		cin >> source >> destination;
+	}
+
+}
 
 int main(int argc, char const *argv[])
 {
-	
+	ReadCase();
+
+
 	return 0;
 }
