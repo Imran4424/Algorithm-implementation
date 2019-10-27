5fe8fccfe4ff4dbf2a68f5f85892977a28397348
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-27 13:11:01 +0600

    graph cycle

--- Graph/GraphCycle/CycleDetectPrintUndirected.cpp
+++ Graph/GraphCycle/CycleDetectPrintUndirected.cpp
@@ -6,12 +6,18 @@ const int vSize = 101;
 
 int path[vSize][vSize];
 
+int color[vSize];
+
 int vertex, edges;
+
+int cycleFound;
 
 void Init()
 {
 	for (int i = 0; i <= vertex; ++i)
 	{
+		color[i] = 0;
+
 		for (int j = 0; j <= edges; ++j)
 		{
 			path[i][j] = 0;
@@ -34,6 +40,55 @@ void ReadCase()
 		// Undirected
 		path[x][y] = 1;
 		path[y][x] = 1;
+	}
+}
+
+int DetectCycle(int current, int currentColor)
+{
+	if (0 == color[current])
+	{
+
+		color[current] = currentColor;
+
+		for (int next = 1; next <= vertex; ++next)
+		{
+			if (path[current][next])
+			{
+				int status = DetectCycle(next, currentColor + 1);
+
+				if (-1 == status)
+				{
+					color[next] = -1;
+				}
+			}
+
+
+			
+		}
+	}
+	else
+	{
+		return color[x];
+	}
+}
+
+void Solve()
+{
+	cycleFound = -1;
+
+	int color = 0;
+
+	for (int x = 1; x <= vertex; ++x)
+	{
+		if (0 == color[x])
+		{
+			cycleFound = DetectCycle(x, color + 1);
+
+			if (-1 != cycleFound)
+			{
+				break;
+			}
+		}
 	}
 }
 
