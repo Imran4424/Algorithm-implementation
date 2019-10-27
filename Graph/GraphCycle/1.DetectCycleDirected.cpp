32539b30ca7c2535ba8774ac139762fba738a022
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-27 09:37:21 +0600

    graph cycle

--- Graph/GraphCycle/1.DetectCycleDirected.cpp
+++ Graph/GraphCycle/1.DetectCycleDirected.cpp
@@ -10,11 +10,69 @@ const int vSize = 50;
 
 int path[vSize][vSize];
 
+bool visited[vSize];
+
 int vertex;
+
+bool isCycleAvailabe;
+
+bool DetectCycle(int current)
+{
+	if(visited[current])
+	{
+		return true;
+	}
+
+	visited[current] = true;
+
+	for (int next = 0; next < vertex; ++next)
+	{
+		if (path[current][next])
+		{
+			return DetectCycle(next);
+		}
+	}
+
+	return false;
+}
+
+
+void Solve()
+{
+	isCycleAvailabe = false;
+
+	for (int x = 0; x < vertex; ++x)
+	{
+		if (!visited[x])
+		{
+			if(DetectCycle(x))
+			{
+				isCycleAvailabe = true;
+
+				break;
+			}
+		}
+	}
+}
+
+void Init()
+{
+	for (int i = 0; i <= vertex; ++i)
+	{
+		visited[i] = false;
+
+		for (int j = 0; j < vertex; ++j)
+		{
+			path[i][j] = 0;
+		}
+	}
+}
 
 void ReadCase()
 {
 	cin >> vertex;
+
+	Init();
 
 	int edges, source, destination;
 
@@ -23,13 +81,34 @@ void ReadCase()
 	while(edges--)
 	{
 		cin >> source >> destination;
+
+		path[source][destination] = 1;
 	}
 
 }
 
+void Display()
+{
+	if (isCycleAvailabe)
+	{
+		cout << "Cycle Found" << endl;
+	}
+	else
+	{
+		cout << "Cycle not Found" << endl;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	ReadCase();
+	freopen("input.txt", "r", stdin);
+
+	while(cin >> vertex && vertex)
+	{
+		ReadCase();
+		Solve();
+		Display();
+	}
 
 
 	return 0;
