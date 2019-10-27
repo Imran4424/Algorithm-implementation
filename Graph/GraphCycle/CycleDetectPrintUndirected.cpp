ebcadfe41c6be29bb493534127336d23eb8cc019
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-27 11:55:02 +0600

    graph cycle

--- Graph/GraphCycle/CycleDetectPrintUndirected.cpp
+++ Graph/GraphCycle/CycleDetectPrintUndirected.cpp
@@ -2,8 +2,50 @@
 #include <stdio.h>
 using namespace std;
 
+const int vSize = 101;
+
+int path[vSize][vSize];
+
+int vertex, edges;
+
+void Init()
+{
+	for (int i = 0; i <= vertex; ++i)
+	{
+		for (int j = 0; j <= edges; ++j)
+		{
+			path[i][j] = 0;
+		}
+	}
+}
+
+void ReadCase()
+{
+	cin >> vertex >> edges;
+
+	int x, y;
+
+	while(edges--)
+	{
+		cin >> x >> y;
+
+		// Undirected
+		path[x][y] = 1;
+		path[y][x] = 1;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	
+	int test;
+	cin >> test;
+
+	for (int t = 1; t <= test; ++t)
+	{
+		printf("#%d ", t);
+
+		ReadCase();
+	}
+
 	return 0;
 }
