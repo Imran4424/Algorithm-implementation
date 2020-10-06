6548ddc67167da8fd706f5d41c5c59a8db39115b
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-06 19:34:38 +0600

    string algorithm

---
+++ String/KMPWhile.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+char mainString[50005];
+int piArray[5];
+
+void kmpPreProcess(char pattern[]) {
+	piArray[0] = 0;
+	int j = 0;
+	int i = 1;
+
+	while (pattern[i]) {
+		if (mainString[i] == pattern[j]) {
+			piArray[i++] = ++j;
+		} else {
+			if(j > 0) {
+				j = piArray[j - 1];
+			}
+		}
+	}
+	
+}
+
+bool kmp(char pattern[]) {
+	int j = 0;
+	for(int i = 0; mainString[i]; i++) {
+		if (mainString[i] == pattern[j]) {
+			if(!pattern[j + 1]) {
+				return true;
+			}
+			j++;
+		} else {
+			while(j > 0) {
+				j = piArray[j - 1];
+				if (mainString[i] == pattern[j]) {
+					j++;
+					break;
+				}
+			}
+		}
+	}
+
+	return false;	
+}
+
+void kmpAllPositions(char pattern[]) {
+	int j = 0;
+	for(int i = 0; mainString[i]; i++) {
+		if (mainString[i] == pattern[j]) {
+			printf("%d - %d ", i, j);
+			
+			if(!pattern[j + 1]) {
+				//printf("%d ", i - j);
+				// for clearing
+				j = 0;
+			} else {
+				j++;
+			}
+		} else {
+			while(j > 0) {
+				j = piArray[j - 1];
+				if (mainString[i] == pattern[j]) {
+					j++;
+					break;
+				}
+			}
+		}
+	}
+}
+
+void kmpReplaceSubstring(char pattern[], char replace[]) {
+	int j = 0;
+	for(int i = 0; mainString[i]; i++) {
+		if (mainString[i] == pattern[j]) {
+			if(!pattern[j + 1]) {
+				int x, y;
+				for (x = i - j, y = 0; replace[y]; x++, y++) {
+					mainString[x] = replace[y];
+				}
+				// for clearing
+				j = 0;
+			} else {
+				j++;
+			}
+			
+		} else {
+			while(j > 0) {
+				j = piArray[j - 1];
+				if (mainString[i] == pattern[j]) {
+					j++;
+					break;
+				}
+			}
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	char pattern[5];
+	char replace[5];
+
+	// scanf("%[^\n]", mainString);
+	// getchar();
+	// scanf("%[^\n]", pattern);
+
+	scanf("%s", mainString, pattern, replace);
+
+	printf("given String - %s\n\n", mainString);
+
+	while(cin >> pattern >> replace) {
+		printf("%s %s\n", pattern, replace);
+		kmpPreProcess(pattern);
+
+		if(kmp(pattern)) {
+			printf("subString found\n");
+		} else {
+			printf("subString not found\n");
+		}
+
+		printf("subString positions: ");
+		kmpAllPositions(pattern);
+		printf("\n\n");
+
+		kmpReplaceSubstring(pattern, replace);
+		printf("%s\n", mainString);
+	}
+
+	return 0;
+}
