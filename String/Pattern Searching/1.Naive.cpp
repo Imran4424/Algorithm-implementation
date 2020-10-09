1ed517e2cfba35090feb9c2211cf0ec470ed7074
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-09 15:47:57 +0600

    pattern matching

--- String/Pattern Searching/1.Naive.cpp
+++ String/Pattern Searching/1.Naive.cpp
@@ -5,6 +5,37 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	char pattern[5];
+	char replace[5];
+
+	// scanf("%[^\n]", mainString);
+	// getchar();
+	// scanf("%[^\n]", pattern);
+
+	scanf("%s", mainString);
+
+	printf("given String - %s\n\n", mainString);
+
+	while(cin >> pattern >> replace) {
+		printf("%s %s\n", pattern, replace);
+		kmpPreProcess(pattern);
+
+		if(kmp(pattern)) {
+			printf("subString found\n");
+		} else {
+			printf("subString not found\n");
+		}
+
+		printf("subString positions: ");
+		kmpAllPositions(pattern);
+		printf("\n\n");
+
+		kmpReplaceSubstring(pattern, replace);
+		printf("%s\n", mainString);
+	}
+
 	return 0;
 }
