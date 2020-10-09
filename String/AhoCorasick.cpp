cc3866ed7ec93eb67e4d32cbe2e3c252f6920b35
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-09 09:33:34 +0600

    string algorithm

--- String/AhoCorasick.cpp
+++ String/AhoCorasick.cpp
@@ -15,6 +15,24 @@ struct node
 	}
 };
 
+node* root = new node();
+
+void insert(char word[]) {
+	node* travel = root;
+
+	for(int i = 0; word[i]; i++) {
+		int id = word[i] - 'a';
+
+		if (travel -> next[id] == NULL) {
+			travel -> next[id] = new node();
+		}
+
+		travel = travel -> next[id];
+	}
+
+	travel -> endMark = true;
+}
+
 
 int main(int argc, char const *argv[])
 {
