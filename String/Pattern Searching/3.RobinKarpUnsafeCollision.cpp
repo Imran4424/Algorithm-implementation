fa86854bc744233be75e679e73edca325fbb15fc
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-13 00:20:16 +0600

    robin karp

--- String/Pattern Searching/3.RobinKarpUnsafeCollision.cpp
+++ String/Pattern Searching/3.RobinKarpUnsafeCollision.cpp
@@ -10,9 +10,20 @@ typedef long long int lli;
 
 char mainString[50005];
 
+int stringLength(char text[]) {
+	int len = 0;
+	for (int i = 0; text[i]; i++) {
+		len++;
+	}
+
+	return len;
+}
+
 lli getHash()
 
-int findMatch()
+int findMatch(char pattern[]) {
+
+}
 
 int main(int argc, char const *argv[])
 {
