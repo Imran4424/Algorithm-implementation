2e1cef70dbc355170dd1fac99c33f9eeed103acc
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-13 15:52:29 +0600

    binary search

--- Searching/2.BinarySearchOptimised.cpp
+++ Searching/2.BinarySearchOptimised.cpp
@@ -2,5 +2,10 @@
 	Problem statement
 	Given a sorted array of N distinct elements. Find a key in the array using least number of comparisons. 
 
-	
+
 */
+
+
+int binarySearch(int arr[], int low, int high, int target) {
+	
+}
